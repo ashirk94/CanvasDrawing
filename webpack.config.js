a620619b7d1e96a8e22546a1d77d4199b540a8a7
Alan Shirk <ashirk94@gmail.com>
2022-01-07 15:36:42 -0800

    finished drawing app

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    context: __dirname,
+    entry: [
+      './src/js/index.js'],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js'
+    },
+    devServer: {  
+      static: './dist',
+      compress: true,  
+      port: 8080
+    }, 
+    devtool: 'source-map', 
+    module: {
+      rules: [	
+        { 
+          test: /\.js$/i,
+          exclude: /(node_modules)/,
+          use: { 
+            loader: 'babel-loader', 
+            options: {
+            presets: ['@babel/preset-env']
+          }}
+        }]
+    },
+    plugins: [  
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.ProvidePlugin(
+            { 
+              jQuery: 'jquery', 
+              $: 'jquery', 
+              jquery: 'jquery' 
+            }), 
+            new HtmlWebpackPlugin({
+              template: './index.html',
+            })
+    ],
+}
+  
