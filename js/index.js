1353ae6d368954d5ddb0f9643368845175ab24af
Alan Shirk <ashirk94@gmail.com>
2022-09-05 23:43:13 -0700

    removed unnecessary dependencies and files

---
+++ js/index.js
@@ -0,0 +1,48 @@
+class Drawing {
+	constructor() {
+		//get canvas element and context
+		this.canvas = document.querySelector('#canvas')
+		this.ctx = this.canvas.getContext('2d')
+
+		//resizing canvas
+		this.canvas.height = window.innerHeight
+		this.canvas.width = window.innerWidth
+
+		//signifies if the user is currently painting
+		this.painting = false
+
+		//binding methods
+		this.startPosition = this.startPosition.bind(this)
+		this.finishedPosition = this.finishedPosition.bind(this)
+		this.draw = this.draw.bind(this)
+
+		//event listeners
+		this.canvas.addEventListener('mousedown', this.startPosition)
+		this.canvas.addEventListener('mouseup', this.finishedPosition)
+		this.canvas.addEventListener('mousemove', this.draw)
+	}
+
+	//start drawing
+	startPosition(e) {
+		this.painting = true
+		this.draw(e)
+	}
+	//stop drawing
+	finishedPosition() {
+		this.painting = false
+		this.ctx.beginPath()
+	}
+	//main drawing function
+	draw(e) {
+		if (!this.painting) return //do nothing if mouse isn't clicked
+
+		this.ctx.lineWidth = 10
+		this.ctx.lineCap = 'round'
+
+		this.ctx.lineTo(e.clientX, e.clientY)
+		this.ctx.stroke()
+		this.ctx.beginPath()
+		this.ctx.moveTo(e.clientX, e.clientY)
+	}
+}
+new Drawing()
